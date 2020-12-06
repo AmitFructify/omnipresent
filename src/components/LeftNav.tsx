12e915d35c8c9fbc0919b3547e1c5920343b949f
Amit Parmar <amit.parmar@livspace.com>
2020-12-06 20:15:17 +0530

    Logo + nav icons added

--- src/components/LeftNav.tsx
+++ src/components/LeftNav.tsx
@@ -2,26 +2,35 @@ import React, { Fragment } from "react";
 import "./LeftNav.scss";
 import { NavLink } from "react-router-dom";
 
+import { ReactComponent as Logo } from "../icons/logo.svg";
+import { ReactComponent as Calendar } from "../icons/calendar.svg";
+import { ReactComponent as Bell } from "../icons/bell.svg";
+import { ReactComponent as Wallet } from "../icons/wallet.svg";
+import { ReactComponent as Truck } from "../icons/truck.svg";
+import { ReactComponent as Report } from "../icons/report.svg";
+import { ReactComponent as Question } from "../icons/question.svg";
+import { ReactComponent as Catalog } from "../icons/catalog.svg";
+
 interface ILeftNavProps {};
 
 const LeftNav: React.FC<ILeftNavProps> = (props: ILeftNavProps) => {
     return (
         <Fragment>
             <div className="appBrand">
-                <div className="logo"></div>
+                <div className="logo"><Logo width="30px" height="30px"/></div>
                 <div className="name">
                     <div className="app">Omni Present</div>
                     <div className="owner">by LIVSPACE</div>
                 </div>
             </div>
             <div className="appNav">
-                <NavLink activeClassName="active" to="/notifications">Notifications</NavLink>
-                <NavLink activeClassName="active" to="/appointments">Appointments</NavLink>
-                <NavLink activeClassName="active" to="/orders">Orders</NavLink>
-                <NavLink activeClassName="active" to="/wallet">Wallet <span>&#x20B9; 1,50,000</span></NavLink>
-                <NavLink activeClassName="active" to="/catalog">Catalog</NavLink>
-                <NavLink activeClassName="active" to="/reports">Reports</NavLink>
-                <NavLink activeClassName="active" to="/help">Help</NavLink>
+                <NavLink activeClassName="active" to="/notifications"><span><Bell width="18px" height="18px"/>Notifications</span></NavLink>
+                <NavLink activeClassName="active" to="/appointments"><span><Calendar width="18px" height="18px"/>Appointments</span></NavLink>
+                <NavLink activeClassName="active" to="/orders"><span><Truck width="18px" height="18px"/>Orders</span></NavLink>
+                <NavLink activeClassName="active" to="/wallet"><span><Wallet width="18px" height="18px"/>Wallet</span> <span className="wallet">&#x20B9; 1,50,000</span></NavLink>
+                <NavLink activeClassName="active" to="/catalog"><span><Catalog width="18px" height="18px"/>Catalog</span></NavLink>
+                <NavLink activeClassName="active" to="/reports"><span><Report width="18px" height="18px"/>Reports</span></NavLink>
+                <NavLink activeClassName="active" to="/help"><span><Question width="18px" height="18px"/>Help</span></NavLink>
             </div>
             <div className="appProfile">
                 <div className="image"></div>
